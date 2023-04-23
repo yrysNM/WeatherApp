0cd579b7a14da0fa9cc9492bc9db7ae6791f1bed
yrysNM <yrys.n3m@gmail.com>
2023-04-23 16:56:26 +0600

    charge chart

--- client/src/components/AverageWeekTemperature/AverageWeekTemperature.tsx
+++ client/src/components/AverageWeekTemperature/AverageWeekTemperature.tsx
@@ -1,110 +1,60 @@
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from "chart.js";
-// import { Line } from "react-chartjs-2";
-// import { faker } from "@faker-js/faker";
-
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   PointElement,
-//   LineElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
-
-// import { ContentLayout } from "../layouts/contentLayout";
-// import "./averageWeekTemperature.scss";
-
-// export const options = {
-//   responsive: true,
-//   plugins: {
-//     legend: {
-//       display: false,
-//     },
-//     title: {
-//       display: false,
-//     },
-//   },
-// };
-
-// const labels = ["Week 1", "Week 2", "Week 3", "Week 4"];
-
-// export const data = {
-//   labels,
-//   datasets: [
-//     {
-//       data: labels.map(() => faker.datatype.number({ min: 10, max: 50 })),
-//       borderColor: "#172E52",
-//       backgroundColor: "rgb(252, 252, 252)",
-//       tension: 0.5,
-//       pointRadius: 5,
-//       pointHoverBackgroundColor: "#172E52",
-//       pointHoverRadius: 10,
-//     },
-//   ],
-// };
-
-// /**
-//  * @featureChange -> recharts
-//  * @returns JSX.ELEMENT
-//  */
-// export const AverageWeekTemperature = () => {
-//   return (
-//     <ContentLayout title="Average Weekly Temperature">
-//       <div className="static">
-//         <Line options={options} data={data} />
-//       </div>
-//     </ContentLayout>
-//   );
-// };
-
 import {
-  LineChart,
-  Line,
+  AreaChart,
+  Area,
   XAxis,
   YAxis,
-  CartesianGrid,
   Tooltip,
-  Legend,
+  CartesianGrid,
+  ResponsiveContainer,
 } from "recharts";
 
+import { ContentLayout } from "../layouts/contentLayout";
+
+import "./averageWeekTemperature.scss";
+
 const data = [
-  { name: "A", value: 10 },
-  { name: "B", value: 20 },
-  { name: "C", value: 30 },
+  { name: "", value: 0 },
+  { name: "week 1", value: 20 },
+  { name: "week 2", value: 30 },
+  { name: "week 3", value: 10 },
+  { name: "week 4", value: 27 },
 ];
 
 export function AverageWeekTemperature() {
   return (
-    <LineChart width={500} height={300} data={data}>
-      <defs>
-        <filter id="boxShadow" x="-20%" y="-20%" width="150%" height="150%">
-          <feOffset result="offOut" in="SourceAlpha" dx="2" dy="2" />
-          <feGaussianBlur result="blurOut" in="offOut" stdDeviation="2" />
-          <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
-        </filter>
-      </defs>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="value"
-        stroke="#8884d8"
-        strokeWidth={2}
-        filter="url(#boxShadow)"
-      />
-    </LineChart>
+    <ContentLayout title="Average Weekly Temperature">
+      <div className="static">
+        <ResponsiveContainer width="100%" height={300}>
+          <AreaChart width={600} height={300} data={data}>
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#172E52" stopOpacity={0.2} />
+                <stop offset="95%" stopColor="#172E52" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.5} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <CartesianGrid
+              strokeDasharray="4 4"
+              horizontal={true}
+              vertical={false}
+            />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="value"
+              stroke="#172E52"
+              strokeWidth={4}
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    </ContentLayout>
   );
 }
