63fe3275a3cb0620639024a7646668dec022c404
yrysNM <yrys.n3m@gmail.com>
2023-04-23 16:15:24 +0600

    added algorithm sunrise and sunset

--- client/src/components/AverageWeekTemperature/AverageWeekTemperature.tsx
+++ client/src/components/AverageWeekTemperature/AverageWeekTemperature.tsx
@@ -1,68 +1,110 @@
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from "chart.js";
+// import { Line } from "react-chartjs-2";
+// import { faker } from "@faker-js/faker";
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+
+// import { ContentLayout } from "../layouts/contentLayout";
+// import "./averageWeekTemperature.scss";
+
+// export const options = {
+//   responsive: true,
+//   plugins: {
+//     legend: {
+//       display: false,
+//     },
+//     title: {
+//       display: false,
+//     },
+//   },
+// };
+
+// const labels = ["Week 1", "Week 2", "Week 3", "Week 4"];
+
+// export const data = {
+//   labels,
+//   datasets: [
+//     {
+//       data: labels.map(() => faker.datatype.number({ min: 10, max: 50 })),
+//       borderColor: "#172E52",
+//       backgroundColor: "rgb(252, 252, 252)",
+//       tension: 0.5,
+//       pointRadius: 5,
+//       pointHoverBackgroundColor: "#172E52",
+//       pointHoverRadius: 10,
+//     },
+//   ],
+// };
+
+// /**
+//  * @featureChange -> recharts
+//  * @returns JSX.ELEMENT
+//  */
+// export const AverageWeekTemperature = () => {
+//   return (
+//     <ContentLayout title="Average Weekly Temperature">
+//       <div className="static">
+//         <Line options={options} data={data} />
+//       </div>
+//     </ContentLayout>
+//   );
+// };
+
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
   Tooltip,
   Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
+} from "recharts";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+const data = [
+  { name: "A", value: 10 },
+  { name: "B", value: 20 },
+  { name: "C", value: 30 },
+];
 
-import { ContentLayout } from "../layouts/contentLayout";
-import "./averageWeekTemperature.scss";
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      display: false,
-    },
-    title: {
-      display: false,
-    },
-  },
-};
-
-const labels = ["Week 1", "Week 2", "Week 3", "Week 4"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      data: labels.map(() => faker.datatype.number({ min: 10, max: 50 })),
-      borderColor: "#172E52",
-      backgroundColor: "rgb(252, 252, 252)",
-      tension: 0.5,
-      pointRadius: 5,
-      pointHoverBackgroundColor: "#172E52",
-      pointHoverRadius: 10,
-    },
-  ],
-};
-
-/**
- * @featureChange -> recharts
- * @returns JSX.ELEMENT
- */
-export const AverageWeekTemperature = () => {
+export function AverageWeekTemperature() {
   return (
-    <ContentLayout title="Average Weekly Temperature">
-      <div className="static">
-        <Line options={options} data={data} />
-      </div>
-    </ContentLayout>
+    <LineChart width={500} height={300} data={data}>
+      <defs>
+        <filter id="boxShadow" x="-20%" y="-20%" width="150%" height="150%">
+          <feOffset result="offOut" in="SourceAlpha" dx="2" dy="2" />
+          <feGaussianBlur result="blurOut" in="offOut" stdDeviation="2" />
+          <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+        </filter>
+      </defs>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line
+        type="monotone"
+        dataKey="value"
+        stroke="#8884d8"
+        strokeWidth={2}
+        filter="url(#boxShadow)"
+      />
+    </LineChart>
   );
-};
+}
--- client/src/utils/helper/CustomDate.ts
+++ client/src/utils/helper/CustomDate.ts
@@ -1,12 +1,8 @@
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+// Define the necessary astronomical constants
+const DEG_TO_RAD = Math.PI / 180;
+const RAD_TO_DEG = 180 / Math.PI;
+const EARTH_RADIUS = 6371; // in km
+const EARTH_ATMOSPHERE_REFRACTION = 34 / 60; // in degrees
 
 export class CustomDate {
   private static days = [
@@ -45,6 +41,96 @@ export class CustomDate {
     const monthName: string = this.months[this.d.getMonth()];
     return monthName;
   };
+
+  // Calculate the solar declination angle for a given day of the year
+  // Вычислить угол склонения солнца для данного дня года
+  private calculateSolarDeclination(dayOfYear: number): number {
+    return (
+      -23.45 * Math.cos((2 * Math.PI * (dayOfYear - 81)) / 365) * DEG_TO_RAD
+    );
+  }
+
+  // Calculate the hour angle for a given location and time of day
+  private calculateHourAngle(
+    longitude: number,
+    solarDeclination: number,
+    latitude: number,
+    zenith: number,
+    isSunrise: boolean
+  ): number {
+    const sinPhi = Math.sin(latitude * DEG_TO_RAD);
+    const cosPhi = Math.cos(latitude * DEG_TO_RAD);
+    const sinDelta = Math.sin(solarDeclination);
+    const cosDelta = Math.cos(solarDeclination);
+    const cosH =
+      (Math.cos(zenith * DEG_TO_RAD) - sinDelta * sinPhi) / (cosDelta * cosPhi);
+    let hourAngle = Math.acos(cosH);
+
+    if (isSunrise) {
+      hourAngle = -hourAngle;
+    }
+
+    return hourAngle;
+  }
+
+  // Calculate the sunrise and sunset times for a given date and location
+  /**
+   *
+   * @param {Date} date - The date for which to calculate the sunrise and sunset times.
+   * @param {number} latitude - The latitude of the location in decimal degrees.
+   * @param {number} longitude - The longitude of the location in decimal degrees.
+   * @returns  {{ sunrise: Date, sunset: Date }} - An object containing the calculated sunrise and sunset times as Date objects.
+   */
+  public getSunriseAndSunset(
+    date: Date,
+    latitude: number,
+    longitude: number
+  ): { sunrise: Date; sunset: Date } {
+    // Calculate the day of the year
+    const dayOfYear = Math.floor(
+      (date.getTime() - Date.UTC(date.getUTCFullYear(), 0, 0, 0, 0, 0, 0)) /
+        86400000
+    );
+
+    // Calculate the solar declination angle
+    const solarDeclination = this.calculateSolarDeclination(dayOfYear);
+
+    // Calculate the zenith angle (i.e. the angle between the observer's
+    // horizon and the astronomical horizon)
+    const zenith = 90 + EARTH_ATMOSPHERE_REFRACTION;
+
+    // Calculate the hour angles for sunrise and sunset
+    const sunriseHourAngle = this.calculateHourAngle(
+      longitude,
+      solarDeclination,
+      latitude,
+      zenith,
+      true
+    );
+    const sunsetHourAngle = this.calculateHourAngle(
+      longitude,
+      solarDeclination,
+      latitude,
+      zenith,
+      false
+    );
+
+    // Calculate the time of sunrise and sunset
+    const utcMidnight = new Date(
+      Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate())
+    );
+    const sunriseTime = new Date(
+      utcMidnight.getTime() + RAD_TO_DEG * sunriseHourAngle * 4 * 60 * 1000
+    );
+    const sunsetTime = new Date(
+      utcMidnight.getTime() + RAD_TO_DEG * sunsetHourAngle * 4 * 60 * 1000
+    );
+
+    return {
+      sunrise: sunriseTime,
+      sunset: sunsetTime,
+    };
+  }
 }
 
 export function cusomDate(): string {
