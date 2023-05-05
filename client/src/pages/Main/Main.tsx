a56a2c7dfd3e108e28916a33e2216932b781c3d6
yrysNM <yrys.n3m@gmail.com>
2023-05-06 00:52:06 +0600

    axios

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -4,8 +4,11 @@ import { useAppDispatch } from "../../hooks/redux.hook";
 import { fetchCurrentCityWeather } from "../../api/weather";
 import { TodayOverview } from "../../components/TodayOverview";
 import { AverageWeekTemperature } from "../../components/AverageWeekTemperature";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Main = () => {
+<<<<<<< Updated upstream
   const dispatch = useAppDispatch();
 
   const fetchDefaultCityWeather = useCallback(() => {
@@ -15,6 +18,13 @@ const Main = () => {
   useEffect(() => {
     fetchDefaultCityWeather();
   }, [fetchDefaultCityWeather]);
+=======
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/weather/almaty")
+      .then((res) => console.log(res.data));
+  }, []);
+>>>>>>> Stashed changes
 
   return (
     <>
