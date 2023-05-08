4eed838a9cac6b30310186a999ae990f366d6da0
yrysNM <yrys.n3m@gmail.com>
2023-05-08 11:56:17 +0600

    fixed graph average temp

--- client/src/components/AverageWeekTemperature/AverageWeekTemperature.tsx
+++ client/src/components/AverageWeekTemperature/AverageWeekTemperature.tsx
@@ -11,22 +11,15 @@ import {useEffect, useState} from 'react';
 
 import {useAppSelector} from '../../hooks/redux.hook';
 import {ContentLayout} from '../layouts/contentLayout';
-import type {List} from '../../Interfaces/ICurrentCityWeather';
 
 import './averageWeekTemperature.scss';
-
-const data = [
-  {name: '', value: 0},
-  {name: 'week 1', value: 20},
-  {name: 'week 2', value: 30},
-  {name: 'week 3', value: 10},
-  {name: 'week 4', value: 27},
-];
 
 export function AverageWeekTemperature() {
   const {list} = useAppSelector((state) => state.weatherDailyForecast);
   const [tempEachDays, setTempEachDays] =
-    useState<{name: string; value: number}[]>();
+    useState<
+      {name: string; kelvin: number; celsius: number; fahrenheit: number}[]
+    >();
 
   useEffect(() => {
     const filterList = list.filter(
@@ -34,16 +27,17 @@ export function AverageWeekTemperature() {
     );
 
     setTempEachDays([
-      {name: '', value: 0},
       ...filterList.map((data) => ({
         name: String(new Date(data.dt_txt).getDate()),
-        value: data.main.temp,
+        kelvin: data.main.temp,
+        celsius: Math.round(data.main.temp - 273.15),
+        fahrenheit: Math.round(1.8 * (data.main.temp - 273.15) + 32),
       })),
     ]);
   }, [list]);
 
   return (
-    <ContentLayout title="Average Weekly Temperature">
+    <ContentLayout title="Average Days Temperature">
       <div className="static">
         <ResponsiveContainer width="100%" height={450}>
           <AreaChart width={600} height={450} data={tempEachDays}>
@@ -67,7 +61,24 @@ export function AverageWeekTemperature() {
             <Tooltip />
             <Area
               type="monotone"
-              dataKey="value"
+              dataKey="kelvin"
+              stroke="#172E52"
+              strokeWidth={4}
+              fillOpacity={1}
+              fill="url(#colorUv)"
+              activeDot={{r: 8}}
+            />
+            <Area
+              type="monotone"
+              dataKey="celsius"
+              stroke="#172E52"
+              strokeWidth={4}
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+            <Area
+              type="monotone"
+              dataKey="fahrenheit"
               stroke="#172E52"
               strokeWidth={4}
               fillOpacity={1}
